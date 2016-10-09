be6d64015598e56214fef6aeecf137f95e59d838
yglazner <yoavglazner@gmail.com>
2016-10-09 11:40:21 +0300

    things for me to remember

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+
+version = '0.4'
+
 setup(
   name = 'kivyoav',
   packages = ['kivyoav'],
-  version = '0.4',
+  version = version,
   description = 'A set of kivy utils and widgets - more details on github ...',
   author = 'Yoav Glazner',
   author_email = 'yoavglazner@gmail.com',
   url = 'https://github.com/yglazner/kivyoav', 
-  download_url = 'https://github.com/yglazner/kivyoav/tarball/0.4', 
+  download_url = 'https://github.com/yglazner/kivyoav/tarball/%s'%version, 
   keywords = ['kivy', 'widget', 'ui'], # arbitrary keywords
   classifiers = [],
 )
